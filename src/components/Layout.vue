3165a185d3caeefd22f6490e1a747bc05d908f4c
Gentrit Jashari <gentijashari5@gmail.com>
2025-10-30 16:43:57 +0100

    feat: move current interface to legacy status

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,55 +1,20 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
-import Sidebar from "@/components/Sidebar.vue";
-import SplitterView from "@/views/splitter/SplitterView.vue";
+import { ref } from "vue";
 import SettingsView from "@/views/SettingsView.vue";
-import ComingSoon from "@/components/ComingSoon.vue";
-import {
-  TOOLS,
-  TOOL_DESCRIPTIONS,
-  DEFAULT_TOOL,
-  type ToolId,
-  type ViewId,
-} from "@/constants/tools";
 
-const activeView = ref<ViewId>(DEFAULT_TOOL);
-
-const currentTool = computed(() => {
-  if (activeView.value === 'settings') {
-    return null;
-  }
-  
-  return {
-    name: TOOLS[activeView.value as ToolId].name,
-    description: TOOL_DESCRIPTIONS[activeView.value as ToolId],
-    icon: TOOLS[activeView.value as ToolId].icon,
-  };
-});
-
-const handleViewChange = (viewId: ViewId) => {
-  activeView.value = viewId;
-};
+const activeView = ref("settings");
 </script>
 
 <template>
   <div class="h-screen flex bg-background">
-    <div class="w-80 flex-shrink-0">
-      <Sidebar :activeView="activeView" @viewChange="handleViewChange" />
-    </div>
-
     <div class="flex-1 flex flex-col min-w-0">
-      <SplitterView 
+      <!-- <SplitterView 
         v-if="activeView === 'splitter'" 
         @navigateToSettings="handleViewChange('settings')"
-      />
-      <SettingsView v-else-if="activeView === 'settings'" />
+      /> -->
 
-      <ComingSoon
-        v-else-if="currentTool"
-        :toolName="currentTool.name"
-        :toolDescription="currentTool.description"
-        :icon="currentTool.icon"
-      />
+
+      <SettingsView v-if="activeView === 'settings'" />
     </div>
   </div>
 </template>
