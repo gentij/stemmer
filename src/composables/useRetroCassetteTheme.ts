c76d0d77ea5e1e4535db98ea1b36cbed215394e0
Gentrit Jashari <gentijashari5@gmail.com>
2025-11-06 14:54:57 +0100

    feat: save retro color theme to localstorage

--- src/composables/useRetroCassetteTheme.ts
+++ src/composables/useRetroCassetteTheme.ts
@@ -2,13 +2,17 @@ import { ref } from "vue";
 import { retroCassetteThemes, getThemeById } from "@/constants/retro/cassete";
 import type { CassetteTheme } from "@/types/retro/cassete.interface";
 
+const STORAGE_KEY = "retro-cassette-theme";
+
 export function useRetroCassetteTheme() {
-  const currentThemeId = ref("purple-dream");
+  const savedThemeId = localStorage.getItem(STORAGE_KEY) || "purple-dream";
+  const currentThemeId = ref(savedThemeId);
   const currentTheme = ref<CassetteTheme>(getThemeById(currentThemeId.value));
 
   function selectTheme(themeId: string) {
     currentThemeId.value = themeId;
     currentTheme.value = getThemeById(themeId);
+    localStorage.setItem(STORAGE_KEY, themeId);
   }
 
   return {
